fac1d8f22eb53176e9eef0e47d35ae6215035fea
JhuneAlfred <avelicaria@me.com>
2018-04-02 23:58:32 -0700

    .

--- projects/array-helper-methods/reduce.js
+++ projects/array-helper-methods/reduce.js
@@ -6,3 +6,19 @@ const sum = numbers.reduce((sum, number) => {
 }, 0)
 
 console.log(sum)
+
+///*******************************************************
+
+const primaryColors = [
+    {color: 'red'},
+    {color: 'yellow'},
+    {color: 'blue'},
+]
+
+const newColors = primaryColors.reduce( (lastValue, primaryColor) => {
+    lastValue.push(primaryColor.color)
+    return lastValue
+}, [])
+
+
+console.log(newColors)
