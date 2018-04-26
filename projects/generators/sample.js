4bd2a6c3bee61cff273acd6464326151bb070675
JhuneAlfred <avelicaria@me.com>
2018-04-25 23:53:46 -0700

    .

--- projects/generators/sample.js
+++ projects/generators/sample.js
@@ -1,11 +1,26 @@
-function* numbers(number) {
-    return number * 2
-    yield
+function* shopping() {
+    //stuff in the sidewalk
+
+
+    //walking down the sidewalk
+
+    //go into the store with cash
+    const stuffFromStore = yield 'cash'
+
+    const stuffFromLaundry = yield 'laundry'
+
+    //walking back home'
+    return [stuffFromStore, stuffFromLaundry]
 }
 
+//stuff in the store
+const gen = shopping()
 
-const gen = numbers(32)
+console.log(gen.next())  //leaving our hose
+console.log(gen.next('groceries')) //leaving the store with groceries
+console.log(gen.next('clean clothes'))
+//console.log(gen.next('pickup clothes'))  //laundry
 
-console.log(gen.next(3))
-gen.number = 3
-console.log(gen.next(1))
+//Output
+// { value: 'cash', done: false }
+// { value: 'groceries', done: true }
