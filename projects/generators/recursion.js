95d1d7e29947d12a002d83467c8e5f2a5b97619c
JhuneAlfred <avelicaria@me.com>
2018-05-03 23:42:04 -0700

    .

--- projects/generators/recursion.js
+++ projects/generators/recursion.js
@@ -1,16 +1,38 @@
+//Note: map or for-each doesn't work with generators
+
 class Comment {
-    constructor(content, children) {
-        this.content = content
-        this.children = children
+  constructor(content, children) {
+    this.content = content
+    this.children = children
+  }
+
+  *[Symbol.iterator]() {
+    yield this.content
+
+    for (let child of this.children) {
+      yield ' - '
+      yield* child
     }
+  }
 }
 
+const grandchildren = [
+  new Comment('grand good comment', []),
+  new Comment('grand bad comment', []),
+  new Comment('grand neutral comment', [])
+]
+
 const children = [
-    new Comment('good comment', []),
-    new Comment('bad comment', []),
-    new Comment('neutral comment', [])   
+  new Comment('good comment', []),
+  new Comment('bad comment', grandchildren),
+  new Comment('neutral comment', [])
 ]
 
 const tree = new Comment('Great post!', children)
 
-console.log(tree)
+const values = []
+for (let value of tree) {
+  values.push(value)
+}
+
+console.log(values)
